f5648ba315e4792adfc7f7ae891cd038d2c9e0a8
lucas <lvcasjm@gmail.com>
2020-12-04 16:40:37 +1030

    recaptcha templating

---
+++ lib/components/Form/Fields/Recaptcha.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <% if (options.siteKey) { %>
+    <vue-recaptcha site-key="<%= options.siteKey %>"></vue-recaptcha>
+    <% } %>
+  </div>
+</template>
+
+<script>
+import VueRecaptcha from 'vue-recaptcha';
+
+export default {
+  name: 'Recaptcha',
+  components: { VueRecaptcha },
+};
+</script>
+
+<style scoped></style>
